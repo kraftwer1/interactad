31620f4c05edeb7011956d682861a68a6230eea2
kraftwer1 <kraftwer1>
2012-09-30 20:13:24 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Interactad::Application.config.secret_token = '9a4c4b952888cd6b7f9dfa9a796d5f145b297549dd786774b07d015db61badb85c3a03ed9f632438b0a12c735dc4ac817ec55cafd1a23d330f3d81e830223391'
